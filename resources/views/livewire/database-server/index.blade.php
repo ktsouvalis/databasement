2391b3545dee304fcb86280e0ee846cbffd20b7f
Konstantinos Tsouvalis <kostas.tsou@gmail.com>
2026-06-10 15:08:42 +0300

    fix: hide toggle when no backup configuration exists

--- resources/views/livewire/database-server/index.blade.php
+++ resources/views/livewire/database-server/index.blade.php
@@ -151,12 +151,14 @@
                                      class="text-success" />
                     @endcan
                     @can('viewForm', $server)
-                        <x-menu-item
-                            :title="$server->backups_enabled ? __('Disable Backup') : __('Enable Backup')"
-                            :icon="$server->backups_enabled ? 'o-pause-circle' : 'o-play-circle'"
-                            wire:click="toggleBackupsEnabled('{{ $server->id }}')"
-                            spinner
-                        />
+                        @if($server->backups->isNotEmpty())
+                            <x-menu-item
+                                :title="$server->backups_enabled ? __('Disable Backup') : __('Enable Backup')"
+                                :icon="$server->backups_enabled ? 'o-pause-circle' : 'o-play-circle'"
+                                wire:click="toggleBackupsEnabled('{{ $server->id }}')"
+                                spinner
+                            />
+                        @endif
                     @endcan
                     @can('viewForm', $server)
                         <x-menu-item :title="__('Edit')" icon="o-pencil"
